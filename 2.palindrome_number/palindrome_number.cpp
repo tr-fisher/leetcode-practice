cb5a46dec5f95fbcfd0397ecc249439320b7b88b
tr-fisher <25521988@gm.uit.edu.vn>
2026-05-13 22:50:16 +0700

    Add longest common prefix solution

---
+++ 2.palindrome_number/palindrome_number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // số âm không phải palindrome
+        if (x < 0)
+            return false;
+
+        string s = to_string(x);
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s[left] != s[right])
+                return false;
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
